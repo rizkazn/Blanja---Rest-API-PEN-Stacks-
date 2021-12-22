55bf7dcd3134e5e4d7970e324402b2a61aa93067
Rizka Zahraini Nurmis <70585701+rizkazn@users.noreply.github.com>
2021-12-23 00:44:15 +0700

    Delete src directory

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('../controllers/auth');
-
-// all routes in here are starting with /auth
-router.post('/', controllers.login);
-
-module.exports = router;
--- src/routes/search.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const controllers = require('../controllers/search')
-
-// all routes in here are starting with /search
-router.get("/", controllers.searchDataProduct)
-
-module.exports = router
